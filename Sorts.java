05e17f3a4398c43e91ccc8e5299a269331bd258d
Jonathan Wu <>
2020-12-16 15:14:33 -0500

    added if array is already sorted

--- Sorts.java
+++ Sorts.java
@@ -1,9 +1,8 @@
 public class Sorts {
-
   public static void bubbleSort(int[] data) {
 
-    int didSort = 0;
     for (int i = 0; i < data.length; i++) {
+      int didSort = 0;
       for (int k = 0; k < data.length-1; k++) {
         if (data[k] > data[k+1]) {
           int tempData = data[k+1];
@@ -11,6 +10,9 @@ public class Sorts {
           data[k+1] = data[k];
           data[k] = tempData;
         }
+      }
+      if (didSort == 0) {
+        i = data.length;
       }
     }
 
