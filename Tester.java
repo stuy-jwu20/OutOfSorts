8b922de20ba4fde72423911210faa6e6475a58d1
Jonathan Wu <jwu20@stuy.edu>
2020-12-16 12:16:07 -0500

    rudimentary test cases

--- Tester.java
+++ Tester.java
@@ -1,17 +1,24 @@
 import java.util.Random;
-
-public class DemoRandom{
+import java.util.ArrayList;
+public class Tester {
   public static void main(String[]args){
 
     Random rng = new Random();
-    for(int i =  0; i < 5; i++ ){
-      System.out.println(rng.nextInt() % 1000);
-    }
-    System.out.println();
-
-    // rng = new Random(100);
     // for(int i =  0; i < 5; i++ ){
     //   System.out.println(rng.nextInt() % 1000);
     // }
+    // System.out.println();
+
+    rng = new Random(100);
+    int[] rngArray = new int[5];
+    ArrayList<Integer> rngData = new ArrayList<Integer>();
+    for (int i =  0; i < 5; i++) {
+      rngArray[i] = (rng.nextInt() % 1000);
+    }
+    Sorts.bubbleSort(rngArray);
+    for (int i = 0; i < rngArray.length; i++) {
+      rngData.add(rngArray[i]);
+    }
+    System.out.println(rngData);
   }
 }
