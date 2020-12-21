510c1db090188795b29a1a4e2ebd3c1046999a95
Jonathan Wu <>
2020-12-20 22:58:31 -0500

    added selection tester

--- Tester.java
+++ Tester.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class Tester {
   public static void main(String[]args){
     bubbleTester();
-
+    //insertionTester();
+    selectionTester();
   }
 
   public static void bubbleTester() {
@@ -43,7 +44,6 @@ public class Tester {
 
         if (equalAmount == testCases) {
           System.out.println("bubbleSort() passed with unsorted and random values!");
-          System.out.println(seconds);
         }
 
     // bubbleSort() if length is 0 //
@@ -136,7 +136,6 @@ public class Tester {
 
         if (equalAmount == testCases) {
           System.out.println("insertionSort() passed with unsorted and random values!");
-          System.out.println(seconds);
         }
 
     // bubbleSort() if length is 0 //
@@ -190,4 +189,97 @@ public class Tester {
         }
 
   }
+
+  public static void bubbleTester() {
+    // bubbleSort() with unsorted and random values //
+
+        int testCases = (int)(Math.random() * 10000);
+        int equalAmount = 0;
+
+        for (int i = 0; i < testCases; i++) {
+          Random rng = new Random();
+          int arraySize = (int)(Math.random() * 1000);
+          int[] rngArray = new int[arraySize];
+          ArrayList<Integer> rngData = new ArrayList<Integer>();
+
+          for(int k =  0; k < arraySize; k++ ){
+            int currentSeedInt = (rng.nextInt() % 1000);
+            rngArray[k] = currentSeedInt;
+            rngData.add(currentSeedInt);
+          }
+
+          Collections.sort(rngData);
+          Sorts.selectionSort(rngArray);
+          ArrayList<Integer> sortedBubbleData = new ArrayList<Integer>();
+
+          for (int data : rngArray) {
+              sortedBubbleData.add(data);
+          }
+
+          if (rngData.equals(sortedBubbleData)) {
+            equalAmount++;
+          } else {
+            System.out.println("They are not equal at case " + i);
+            System.out.println("Expected: \n" + rngData);
+            System.out.println("Actual: \n" + Arrays.toString(rngArray));
+          }
+
+        }
+
+        if (equalAmount == testCases) {
+          System.out.println("selectionSort() passed with unsorted and random values!");
+        }
+
+    // bubbleSort() if length is 0 //
+
+        int[] zeroCases = new int[0];
+        ArrayList<Integer> zeroData = new ArrayList<Integer>();
+        ArrayList<Integer> sorted0Data = new ArrayList<Integer>();
+        Sorts.selectionSort(zeroCases);
+        Collections.sort(sorted0Data);
+
+        for (int data : zeroCases) {
+            sorted0Data.add(data);
+        }
+
+        if (zeroData.equals(sorted0Data)) {
+          System.out.println("selectionSort() works with 0 data!");
+        } else {
+          System.out.println("They are not equal with 0 cases.");
+          System.out.println("Expected: \n" + zeroData);
+          System.out.println("Actual: \n" + Arrays.toString(zeroCases));
+        }
+
+    // bubbleSort() if length is 1 //
+
+        int singleEqualAmount = 0;
+        for (int l = 0; l < testCases; l++) {
+          int[] oneCase = new int[1];
+          int singleCase = (int)(Math.random() * 1000);
+          oneCase[0] = singleCase;
+          ArrayList<Integer> oneData = new ArrayList<Integer>();
+          oneData.add(singleCase);
+          ArrayList<Integer> sorted1Data = new ArrayList<Integer>();
+          Sorts.selectionSort(oneCase);
+          Collections.sort(sorted1Data);
+
+          for (int data : oneCase) {
+              sorted1Data.add(data);
+          }
+
+          if (oneData.equals(sorted1Data)) {
+            singleEqualAmount++;
+          } else {
+            System.out.println("They are not equal with 1 cases.");
+            System.out.println("Expected: \n" + oneData);
+            System.out.println("Actual: \n" + Arrays.toString(oneCase));
+          }
+        }
+
+        if (singleEqualAmount == testCases) {
+            System.out.println("selectionSort() works with 1 data!");
+        }
+
+  }
+
 }
