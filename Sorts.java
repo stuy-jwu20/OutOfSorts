63e1593319bd21075e86f1a39cb418596a311e76
Jonathan Wu <>
2020-12-20 22:21:21 -0500

    added insertionSort method

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,5 @@
 public class Sorts {
   public static void bubbleSort(int[] data) {
-
     for (int i = 0; i < data.length && data.length > 1; i++) {
       int didSort = 0;
       for (int k = 0; k < data.length-1; k++) {
@@ -17,11 +16,28 @@ public class Sorts {
     }
   }
 
-  public static void selectionSort(int[] data) {
-
-  }
+  // public static void selectionSort(int[] data) {
+  //   for (int i = 0; i < data.length && data.length > 1; i++) {
+  //
+  //   }
+  // }
 
   public static void insertionSort(int[] data){
-
+    for (int i = 1; i < data.length && data.length > 1; i++) {
+      int currentVal = data[i];
+      for (int k = 0; i < data.length; k++) {
+        if (currentVal < data[k]) {
+          int counter = k;
+          int toReplace = data[k];
+          while (counter < i) {
+            int secondReplace = data[counter+1];
+            data[counter+1] = toReplace;
+            toReplace = secondReplace;
+            counter++;
+          }
+          data[k] = currentVal;
+        }
+      }
+    }
   }
 }
