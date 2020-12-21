37750042d71a2392b0f3ddd7febe557fed59a923
Jonathan Wu <>
2020-12-20 23:49:24 -0500

    fixed bugs for selectionSort()

--- Sorts.java
+++ Sorts.java
@@ -17,33 +17,37 @@ public class Sorts {
   }
 
   public static void selectionSort(int[] data) {
-    for (int i = 0; i < data.length && data.length > 1; i++) {
-      int initialVal = data[i];
-      int toSwap;
-      for (int k = i + 1 ; k < data.length - 1; k++) {
-        if (data[k] < initialVal) {
+    for (int i = 0; data.length > 1 && i < data.length; i++) {
+      int toSort = data[i];
+      int lowestData = data[i];
+      int toSwap = -1;
+      for (int k = i; k < data.length; k++) {
+        if (data[k] < lowestData) {
+          lowestData = data[k];
           toSwap = k;
         }
       }
-      data[i] = data[toSwap];
-      data[toSwap] = lowVal;
+      if (toSwap != -1) {
+        data[i] = lowestData;
+        data[toSwap] = toSort;
+      }
     }
   }
 
-  // public static void insertionSort(int[] data){
-  //   for (int i = 1; i < data.length && data.length > 1; i++) {
-  //     int currentVal = data[i];
-  //     for (int k = 0; k < data.length; k++) {
-  //       if (currentVal < data[k]) {
-  //         int counter = 0;
-  //         int toReplace = data[k];
-  //         while (counter < i) {
-  //           data[counter+1] = data[counter];
-  //           counter++;
-  //         }
-  //         data[k] = currentVal;
-  //       }
-  //     }
-  //   }
-  // }
+  public static void insertionSort(int[] data){
+    for (int i = 1; i < data.length && data.length > 1; i++) {
+      int currentVal = data[i];
+      for (int k = 0; k < data.length; k++) {
+        if (currentVal < data[k]) {
+          int counter = 0;
+          int toReplace = data[k];
+          while (counter < i) {
+            data[counter+1] = data[counter];
+            counter++;
+          }
+          data[k] = currentVal;
+        }
+      }
+    }
+  }
 }
